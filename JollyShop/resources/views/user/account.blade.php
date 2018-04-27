b9be20f38ab2421d6fe95de953e5e190b519935c
maxhaibara <maxhaibara97@gmail.com>
2018-04-27 12:23:59 +0700

    update

--- JollyShop/resources/views/user/account.blade.php
+++ JollyShop/resources/views/user/account.blade.php
@@ -1,28 +1,36 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>JollyShop</title>
-</head>
-<h1>Your Account</h1>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Your Account</div>
+                <div class="card-body">
+                    <table class="table">
+						<tr>
+							<th>User ID</th>
+							<td>{{ $user->id }}</td>
+						</tr>
+						<tr>
+							<th>Nama</th>
+							<td>{{ $user->name }}</td>
+						</tr>
+						<tr>
+							<th>Email</th>
+							<td>{{ $user->email }}</td>
+						</tr>
+						<tr>
+							<th>Balance</th>
+							<td>{{ $user->balance }}</td>
+						</tr>
+					</table>
+					<a href="/home">Go Back To Home</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <body>
-<table border="1">
-	<tr>
-		<th>User ID</th>
-		<td>{{ $user->id }}</td>
-	</tr>
-	<tr>
-		<th>Nama</th>
-		<td>{{ $user->name }}</td>
-	</tr>
-	<tr>
-		<th>Email</th>
-		<td>{{ $user->email }}</td>
-	</tr>
-	<tr>
-		<th>Balance</th>
-		<td>{{ $user->balance }}</td>
-	</tr>
-</table>
-<a href="/home">Go Back To Home</a>
 </body>
-</html>
+@endsection
